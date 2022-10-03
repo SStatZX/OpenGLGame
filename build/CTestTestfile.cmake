55d1e2423e6e1f771797e544a4731d5447750aeb
Lewis <42219873+SStatZX@users.noreply.github.com>
2022-10-03 23:18:12 +0100

    Added GLEW and configured cmake

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/lewisg/Documents/Computer Science/C++/OpenGLGame
+# Build directory: /home/lewisg/Documents/Computer Science/C++/OpenGLGame/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("include")
+subdirs("dependencies/glew-2.2.0/include")
---
+++ build/include/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/lewisg/Documents/Computer Science/C++/OpenGLGame/include
+# Build directory: /home/lewisg/Documents/Computer Science/C++/OpenGLGame/build/include
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
