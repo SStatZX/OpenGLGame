54613911c3501c3710e09196f1a3ac9fd7d88923
Lewis <42219873+SStatZX@users.noreply.github.com>
2022-10-04 21:51:09 +0100

    Fixed find GLEW in CMake

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <GL/glew.h>
+#include <GL/glut.h>
 #include <GLFW/glfw3.h>
 
 int main(void)
 {
+    GLenum err = glewInit();
+    if (GLEW_OK != err)
+    {
+    /* Problem: glewInit failed, something is seriously wrong. */
+    fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
+    }
+    fprintf(stdout, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
+    
     GLFWwindow* window;
 
     /* Initialize the library */
